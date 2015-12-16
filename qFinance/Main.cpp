6c2a602d7a17ee8ae1e8647bcba8835c645de11c
mye6 <maoye6@gmail.com>
2015-12-16 04:00:45 -0500

    count how many lines of code within directory added, #lines: 7020, 12/16/2015

--- qFinance/Main.cpp
+++ qFinance/Main.cpp
@@ -4,26 +4,6 @@
 #include "Leetcode.h"
 
 
-/*
-int count_lines(const string& filename) {
-	int res = 0;
-	ifstream in(filename);
-	string line;
-	while (getline(in, line)) ++res;	
-	in.close();
-	return res;
-}
-
-int count_lines(const vector<string>& files) {
-	int total = 0;
-	for (size_t i = 0; i < files.size(); ++i) {
-		total += count_lines(files[i]);
-	}
-	return total;
-}
-*/
-
-
 int main() {
 	PRINT(count_lines());
 	system("pause");
