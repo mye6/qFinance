6b6a08e50d67867e5a5c951bb9794d545a64f38b
mye6 <maoye6@gmail.com>
2016-01-08 23:17:56 -0500

    Leetcode, #lines: 9715, 1/8/2016

--- qFinance/Main.cpp
+++ qFinance/Main.cpp
@@ -41,7 +41,6 @@ bool isPowerOfTwo5(int n) {
 	return n>0 && (n == 1 || (n % 2 == 0 && isPowerOfTwo5(n / 2)));
 }
 
-
 // 326. Power of Three, module
 bool isPowerOfThree1(int n) {
 	return (n > 0) && (1162261467 % n == 0);
@@ -68,15 +67,49 @@ bool isPowerOfThree4(int n) {
 	return n>0 && (n == 1 || (n % 3 == 0 && isPowerOfThree4(n/3)) );
 }
 
-// 326. Power of Three, 
+// 67. Add Binary
+string addBinary0(string a, string b) {
+	string s = "";
+	int c = 0, i = a.size() - 1, j = b.size() - 1;
+	while (i >= 0 || j >= 0 || c == 1) {
+		c += (i >= 0 ? a[i--] - '0' : 0);
+		c += (j >= 0 ? b[j--] - '0' : 0);		
+		s = char(c % 2 + '0') + s;		
+		c /= 2;
+	}
+	return s;
+}
+
+// 8. String to Integer (atoi)
+int atoi0(const char *str) {
+	int sign = 1, base = 0, i = 0;
+	while (str[i]==' ') ++i;
+	if (str[i]=='-' || str[i]=='+') sign = 1 - 2*(str[i++]=='-');
+	while (str[i] >= '0' && str[i] <= '9') {
+		if (base > INT_MAX/10 || (base == INT_MAX/10 && str[i]-'0'>7)) {
+			if (sign == 1) return INT_MAX;
+			else return INT_MIN;
+		}
+		base = 10 * base + (str[i++] - '0');
+	}
+	return base * sign;
+}
+
+// 223. Rectangle Area
+int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+	int left = max(A, E), right = max(min(C, G), left);
+	int bottom = max(B, F), top = max(min(D, H), bottom);
+	return (C - A)*(D - B) - (right - left)*(top - bottom) + (G - E)*(H - F);
+}
 
 
 
 
 int main(){
-	PRINT(fractionToDecimal(2, 3));
-	PRINT(count_lines());
-	
+	string a = "1"; string b = "101";
+	string res = addBinary0(a, b);
+	PRINT(res);
+
 	system("pause");
 	return 0;
 }  
